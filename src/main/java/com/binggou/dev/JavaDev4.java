02e8ac705469a3f8c97c948aa77df594b4db9b0f
wuhaisheng <wuhaisheng@binggou.com>
2018-02-27 11:16:24 +0800

    武海升第一次写了代码

---
+++ src/main/java/com/binggou/dev/JavaDev4.java
@@ -0,0 +1,18 @@
+package com.binggou.dev;
+
+/**
+ * @author 武海升
+ * @version 2.0
+ * @description
+ * @date 2018-02-27 11:14
+ */
+public class JavaDev4 {
+
+    public static void main(String[] args) {
+        System.out.println("武海升写了代码...");
+    }
+
+    public static void push0() {
+        System.out.println("武海升第一次写了代码...");
+    }
+}
