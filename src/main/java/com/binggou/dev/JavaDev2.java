ede05ad66736239c4daa4eec5f84482e40d5164d
wuhaisheng <wuhaisheng@binggou.com>
2018-02-27 10:30:55 +0800

    开发分支提交代码

---
+++ src/main/java/com/binggou/dev/JavaDev2.java
@@ -0,0 +1,13 @@
+package com.binggou.dev;
+
+/**
+ * @author 武海升
+ * @version 2.0
+ * @description
+ * @date 2018-02-27 10:30
+ */
+public class JavaDev2 {
+    public static void main(String[] args) {
+        System.out.println("开发分支下的武海升提交代码");
+    }
+}
