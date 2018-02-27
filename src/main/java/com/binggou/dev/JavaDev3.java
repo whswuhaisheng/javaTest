821bdfe6a4a2a3b01dfa62fa3797b43d27bfc886
wuhaisheng <wuhaisheng@binggou.com>
2018-02-27 11:10:09 +0800

    Merge remote-tracking branch 'remotes/origin/whs' into dev
    # Conflicts:
    #	src/main/java/com/binggou/dev/JavaDev2.java

---
+++ src/main/java/com/binggou/dev/JavaDev3.java
@@ -0,0 +1,13 @@
+package com.binggou.dev;
+
+/**
+ * @author 武海升
+ * @version 2.0
+ * @description
+ * @date 2018-02-27 11:09
+ */
+public class JavaDev3 {
+    public static void main(String[] args) {
+        System.out.println("开发分支下的其他人提交代码...");
+    }
+}
