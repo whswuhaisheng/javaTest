4185a707c9f9af673d25e02128f830ae7c2ae633
wuhaisheng <wuhaisheng@binggou.com>
2018-02-27 10:24:23 +0800

    主分支提交代码

---
+++ src/main/java/com/binggou/master/JavaMaster.java
@@ -0,0 +1,15 @@
+package com.binggou.master;
+
+/**
+ * @author 武海升
+ * @version 2.0
+ * @description
+ * @date 2018-02-27 10:22
+ */
+public class JavaMaster {
+
+    public static void main(String[] args) {
+        System.out.println("主分支提交代码");
+    }
+
+}
