e9c03f21c218c1e3a5324be2b2974dd278e594f3
wuhaisheng <wuhaisheng@binggou.com>
2018-02-27 10:26:16 +0800

    开发分支提交代码

---
+++ src/main/java/com/binggou/dev/JavaDev1.java
@@ -0,0 +1,13 @@
+package com.binggou.dev;
+
+/**
+ * @author 武海升
+ * @version 2.0
+ * @description
+ * @date 2018-02-27 10:25
+ */
+public class JavaDev1 {
+    public static void main(String[] args) {
+        System.out.println("开发分支提交代码");
+    }
+}
